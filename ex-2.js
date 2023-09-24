a85f2d77760936caa2a2dcdded6546277faf47ad
sirithanatharathorntv <sirithanatharathorntv@gmail.com>
2023-09-24 18:22:41 +0700

    create branch pop

--- ex-2.js
+++ ex-2.js
@@ -12,7 +12,28 @@ const employees = [
   },
 ];
 // Start coding here
-let alex;
-let alexHobbies;
-let alexAndJamesAge;
+let alex = employees[1].name;
+console.log(alex);
+employees[1].hobbies.push("Watching basketball");
+console.log(employees);
+let alexHobbies = employees[1].hobbies;
+console.log(alexHobbies);
+let alexAndJamesAge = employees[0].age + employees[1].age;
+console.log(alexAndJamesAge);
 const updateEmployees = employees;
+updateEmployees[2] =
+{
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading novels"]
+};
+updateEmployees[3] =
+{
+  name: "Kody",
+  age: 19,
+  hobbies: ["Computer games", "Wakeboard"]
+};
+console.log(updateEmployees);
+updateEmployees.pop();
+console.log(updateEmployees);
+console.log(employees);
